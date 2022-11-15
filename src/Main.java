5402f73533d7db1c1ca587a15d6e67659599a1b5
unknown <BT_3W2_03@BKTECH.LOCAL>
2022-11-15 12:31:21 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int result = 0;
+        int m = 0;
+        while (m < 3) {
+            int k = 0;
+            while (k < 4) {
+                result++;
+                k++;
+                System.out.print("k: " + k);
+                System.out.println(", m: " + m);
+            }
+            m++;
+        }
+        System.out.println("result: " + result);
+
+    }
+}
+
